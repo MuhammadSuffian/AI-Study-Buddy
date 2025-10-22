3652a19c255c1ca0f71671c8776c9de25292b15d
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2025-10-22 19:12:17 +0500

    Update AI-Study-Buddy.py

--- AI-Study-Buddy.py
+++ AI-Study-Buddy.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.docstore.document import Document
+from langchain_core.documents import Document
 from langchain.prompts import PromptTemplate
 from groq import Groq
 import PyPDF2
